302db5c9d95eff25b33dd768bf08586a1bb1d24d
Ryan Sabo <sabora920@gmail.com>
2017-11-06 16:58:34 -0500

    added submit btn functionality and question index

--- index.js
+++ index.js
@@ -1,49 +1,57 @@
 'use strict';
 
-const STORE = [
-  {
-    quest: 'What fruit is traditionally used in an Eve\'s pudding?',
-    correctAnswer: 'apples',
-    answersArr: [ 'pears', 'apples', 'raisins', 'lemon'],
-    answersObj: { answer1: 'pears', answer2: 'apples', answer3: 'raisins', answer4: 'lemon' },
-    userAnswerCorrect: false
-  },
+const STORE = 
+    {
+        questions: [
+            {
+                quest: 'What fruit is traditionally used in an Eve\'s pudding?',
+                correctAnswer: 'apples',
+                answersArr: [ 'pears', 'apples', 'raisins', 'lemon'],
+                userAnswerCorrect: false
+              },
+            
+              {
+                quest: 'In which country did Tea originate?',
+                correctAnswer: 'China',
+                answersArr: [ 'China', 'Japan', 'Russia', 'Italy'],
+                userAnswerCorrect: false
+              }
+        ],
+        currentQuestion: 0,
+        score: 0
+    }
+    
 
-  {
-    quest: 'In which country did Tea originate?',
-    correctAnswer: 'China',
-    answersArr: [ 'China', 'Japan', 'Russia', 'Italy'],
-    answersObj: { answer1: 'China', answer2: 'Japan', answer3: 'Russia', answer4: 'Italy' },
-    userAnswerCorrect: false
-  }
-
-];
 
 
-function generateQuestions(currQuestionObj, questionIndex){
-  return `
+function generateQuestions(currQuestionArr, questionIndex){
+  
+  const currentQuestionObj = currQuestionArr[questionIndex];
+  console.log(currentQuestionObj);
+
+    return `
     <div class="placeAndScore hidden">
-        <div id="placeInQuiz">Question ${questionIndex} out of ${STORE.length}</div>
+        <div id="placeInQuiz">Question ${questionIndex + 1} out of ${currQuestionArr.length}</div>
         <div id="scoreInQuiz">Score: 0/10</div>
     </div>
     <div class="questionsAndAnswers">
-        <span>${currQuestionObj.quest}</span>
+        <span>${currentQuestionObj.quest}</span>
     </div>
     <div class="questionsAndAnswers">  
         <input type="radio" name="multipleChoice" id="multipleChoice1">
-        <label for="multipleChoice1">${currQuestionObj.answersArr[0]}</label>
+        <label for="multipleChoice1">${currentQuestionObj.answersArr[0]}</label>
     </div>
     <div class="questionsAndAnswers">
         <input type="radio" name="multipleChoice" id="multipleChoice2">
-        <label for="multipleChoice2">${currQuestionObj.answersArr[1]}</label>
+        <label for="multipleChoice2">${currentQuestionObj.answersArr[1]}</label>
     </div>
     <div class="questionsAndAnswers">
         <input type="radio" name="multipleChoice" id="multipleChoice3">
-        <label for="multipleChoice3">${currQuestionObj.answersArr[2]}</label>
+        <label for="multipleChoice3">${currentQuestionObj.answersArr[2]}</label>
     </div>
     <div class="questionsAndAnswers">
         <input type="radio" name="multipleChoice" id="multipleChoice4">
-        <label for="multipleChoice4">${currQuestionObj.answersArr[3]}</label>
+        <label for="multipleChoice4">${currentQuestionObj.answersArr[3]}</label>
     </div>
     <div>
         <button id="submitAnswerButton">Submit Answer</button>
@@ -55,16 +63,21 @@ function renderHTMLQuestion(strQuestion){
 }
 
 function handleQuestions(storeData){
-  const storeCopy = storeData;
-  const html = generateQuestions(storeCopy[1], questionIndex);
+  const html = generateQuestions(storeData.questions, storeData.currentQuestion);
   renderHTMLQuestion(html);
   //$( "input[type=radio][name=nultipleChoice]:checked" ).val();
 }
 
-
+function handleSubmitAnswer(){
+    $('#submitAnswerButton').on('click', function(event){
+        STORE.currentQuestion++;
+        handleQuestions(STORE);
+    });
+}
 
 function quizApp(){
   handleQuestions(STORE);
+  handleSubmitAnswer();
 }
 
 $(quizApp);
